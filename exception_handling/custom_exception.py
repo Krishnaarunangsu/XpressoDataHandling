60545ffadbd5d380c75e05f84c3f9145717fb720
Krishnaarunangsu <arunangsutech@gmail.com>
2019-08-01 16:44:25 +0530

    First Commit

---
+++ exception_handling/custom_exception.py
@@ -0,0 +1,16 @@
+# A python program to create user-defined exception
+# class MyError is derived from super class Exception
+import Exception
+
+class InvalidDataTypeException(Exception):
+
+    # Constructor or Initializer
+    def __init__(self, value):
+        self.value = value
+
+        # __str__ is to print() the value
+
+    def __str__(self):
+        return repr(self.value)
+
+
